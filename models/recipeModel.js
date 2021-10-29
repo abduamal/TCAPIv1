55dbadf812be96d1640f155ce919cb57a5d39e56
Amal Abdu <abduamal@ymail.com>
2021-10-29 15:05:19 -0500

    set up recipeModel w/ Mongoose + recipes route with database data

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const recipeModel = new Schema(
-  recipe_title: { type:String },
-  author: { type:String },
-  food_type: { type:String },
-  cuisine: { type:String },
-  main_ingredients: { type:String },
-  url: { type:String },
-  diet_restrictions: Schema.Types.Mixed,
-  nutritional_info: Schema.Types.Mixed,
-  occasion: { type:String },
-  way_of_cooking: { type:String },
-  time: Schema.Types.Mixed
+  {
+    recipe_title: { type: String },
+    author: { type: String },
+    food_type: { type: String },
+    cuisine: { type: String },
+    main_ingredients: { type: String },
+    url: { type: String },
+    diet_restrictions: Schema.Types.Mixed,
+    nutritional_info: Schema.Types.Mixed,
+    occasion: { type: String },
+    way_of_cooking: { type: String },
+    time: Schema.Types.Mixed
+  }
 );
+
+module.exports = mongoose.model('Recipe', recipeModel);
