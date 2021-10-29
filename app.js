c6667724356a1d6dfc60c5118290dd5204ed9ba1
Amal Abdu <abduamal@ymail.com>
2021-10-29 15:43:31 -0500

    Install + require body-parser

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express');
 // add mongoose, which will deal with all the database stuff
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // express has been declared, and can be called
 const app = express();
 // connect mongoose to MongoDB
 const db = mongoose.connect('mongodb://localhost/recipeAPI');
 const Recipe = require('./models/recipeModel');
+
 const recipeRouter = express.Router();
 // later, there will be a tool that passes the port into the application.
 // Until it is configured, 3000 serves as backup
