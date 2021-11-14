4853b8da45a61152f7f83721782a257bf3e095c1
Amal Abdu <abduamal@ymail.com>
2021-11-13 19:51:57 -0600

    describe and send mock POST request

--- tests/recipesIntegrationTests.js
+++ tests/recipesIntegrationTests.js
@@ -8,3 +8,19 @@ const app = require('../app');
 
 const Recipe = mongoose.model('Recipe');
 const agent = request.agent(app);
+
+describe('Recipe CRUD Test', () => {
+  it('should allow a recipe to be posted and return _id', (done) => {
+    const recipePost = {
+      recipe_title: 'Awesome Tasting Dish', author: 'Njeshe Amal', food_type: 'breakfast', cuisine: 'American', main_ingredients: 'Eggs, Bacon, Flour, Sugar', url: 'https://falserecipeaddress.com', diet_restrictions: 'all a dem', nutritional_info: 'none a dem', occasion: 'integration testing', way_of_cooking: 'hot air', time: '60 seconds'
+    };
+    agent.post('/api/recipes')
+      .send(recipePost)
+      .expect(200)
+      .end((error, results) => {
+        // console.log(results);
+        // results.body.recipe_title.should.not.equal('Awesome Tasting Dish');
+        results.body.should.have.property('_id');
+        done();
+      });
+  });
